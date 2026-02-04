c2e0260d69fe8ad804c9f699ba091a2db75ce5ba
agentikxai <git@aigroup.app>
2026-02-04 11:06:40 +0200

    latest-commit

---
+++ assessment.js
@@ -0,0 +1,29 @@
+//Question_1 
+
+let student ={
+  name:"Motheo",
+  age:20,
+  course:"Software Engineering",
+  student_number:"2026203"
+};
+console.log("Question 1:" +" "+student.course);
+
+//Question_2
+let person = {
+    name:"teos",
+   contact:{email:"teos@example.com",
+            phone_number:"1234567890"}
+};
+
+console.log("Question 2:" +" "+person.contact.email);
+
+//Question_3
+let students = {
+    student_1:{name:"Master",
+              age:22},
+
+    student_2:{name:"Hope",
+               age:20}
+};
+
+console.log("Question 3:" +" "+ students.student_2.name);
