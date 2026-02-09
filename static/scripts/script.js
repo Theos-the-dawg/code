a3afab10935334cbc7392bcf188a1d991aa95247
mastermasso <massomaster24@gmail.com>
2026-02-09 11:54:20 +0200

    update-date-on-the-latest-code-changes

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,32 +1,45 @@
 //console.log("hello");
 
-let student = {
-    name:"master",
-    time:"2026-02-09 11:07:00"
+//nested dict contains student values
+let students = {
+   student_01:{ name:"master",
+                time: new Date("2026-02-08 12:57:00")},
+
+   student_02:{ name:"Gerald",
+                time:"2026-02-08 12:57:00"}
 };
 
-student.time = new Date();
-console.log(student.time)
-console.log(typeof(student.time))
+
+//
+console.log(typeof(students.student_01.time));
+console.log(students.student_01.time);
+console.log(typeof(students.student_02.time));
+console.log(students.student_02.time = new Date());
+console.log(typeof(students.student_02.time));
+
+
+// console.log(typeof(student.time))
+
+// let day = new Date();
 
 
 
 
-if(student.time>="2026-02-09 11:07:00"){
+// if(student.time.getHours()==12){
 
-    conosle.log(student.name+ " "+"is on time ");
-}else{
-    console.log(student.name+ " " + "is not on time");
-}
+//    console.log(student.name+ " "+"is on time ");
+// }else{
+//     console.log(student.name+ " " + "is not on time");
+// }
 
-let today =function(){
-    let date= new  Date();
-    console.log(date.getHours());
-    date.getHours() + date.getHours()+1;
-    //date.getDay()
-    return date;
+// let today =function(){
+//     let date= new  Date();
+//     console.log(date.getHours());
+//     date.getHours() + date.getHours()+1;
+//     //date.getDay()
+//     return date;
     
 
-};
-console.log(today());
-//document.getElementById("masso").innerHTML =today();
+// };
+// console.log(today());
+// //document.getElementById("masso").innerHTML =today();
