89db31cfebcd46d010a0aa59c11f1c2c1c9b5ba5
mastermasso <massomaster24@gmail.com>
2026-02-09 11:22:08 +0200

    code-changes-on-script

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,32 @@
-console.log("hello")
-document.getElementById("masso").innerHTML ="Master";
+//console.log("hello");
 
+let student = {
+    name:"master",
+    time:"2026-02-09 11:07:00"
+};
+
+student.time = new Date();
+console.log(student.time)
+console.log(typeof(student.time))
+
+
+
+
+if(student.time>="2026-02-09 11:07:00"){
+
+    conosle.log(student.name+ " "+"is on time ");
+}else{
+    console.log(student.name+ " " + "is not on time");
+}
+
+let today =function(){
+    let date= new  Date();
+    console.log(date.getHours());
+    date.getHours() + date.getHours()+1;
+    //date.getDay()
+    return date;
+    
+
+};
+console.log(today());
+//document.getElementById("masso").innerHTML =today();
